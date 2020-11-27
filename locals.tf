4208a3e02ffda868ef52dabdad9c47590b0a482c
drfaust92 <ilia.lazebnik@gmail.com>
2020-11-27 18:25:51 +0200

    extract to local vars + update versions

--- locals.tf
+++ locals.tf
@@ -1,15 +1,17 @@
 locals {
-  controller_name = "ebs-csi-controller"
-  daemonset_name  = "ebs-csi-node"
-  csi_volume_tags = join(",", [for key, value in var.tags : "${key}=${value}"])
+  ebs_csi_driver_version = "v0.7.1"
+  liveness_probe_version = "v2.1.0"
+  controller_name        = "ebs-csi-controller"
+  daemonset_name         = "ebs-csi-node"
+  csi_volume_tags        = join(",", [for key, value in var.tags : "${key}=${value}"])
 
   resizer_container = var.enable_volume_resizing ? [{
     name  = "csi-resizer",
-    image = "quay.io/k8scsi/csi-resizer:v0.5.0"
+    image = "quay.io/k8scsi/csi-resizer:v1.0.1"
   }] : []
 
   snapshot_container = var.enable_volume_snapshot ? [{
     name  = "csi-snapshotter",
-    image = "quay.io/k8scsi/csi-snapshotter:v2.1.1"
+    image = "quay.io/k8scsi/csi-snapshotter:v3.0.2"
   }] : []
 }
